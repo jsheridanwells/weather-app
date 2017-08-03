ec58e623cb8bb36f545f7470e651c2669e6c4b46
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-03 16:47:02 -0500

    capitalized words in description

--- js/calculations.js
+++ js/calculations.js
@@ -4,5 +4,9 @@ var Weather = (function(obj){
 		console.log("calculations working");
 		return ((parseInt(tempK) * 9)/5 - 459.67).toFixed(1);
 	};
+
+	obj.capitalize = function(str) {
+		return str.replace(/\b\w/g, l => l.toUpperCase());
+	};
 	return obj;
 }(Weather || {}));
--- js/dom.js
+++ js/dom.js
@@ -11,12 +11,11 @@ var Weather = (function(obj){
 		            <h2>${Weather.getDay(0)}</h2>
 		            <img src="img/${data.weather[0].icon}.svg" alt="sunny">
 		            <h3>${(Weather.getTempToFahr(data.main.temp))} &deg;</h3>
-		            <h3>${data.weather[0].description}</h3>
+		            <h3>${Weather.capitalize(data.weather[0].description)}</h3>
 		          </div>
 		        </div>
 			`;
 		} else if (type === 'five-day') {
-			console.log("going to five day");
 			let content = `
 				<div class="row">
 		          <div class="col-md-12"><h2>Forecast For ${data.city.name}</h2></div>
@@ -28,18 +27,29 @@ var Weather = (function(obj){
 			            <h2>${Weather.getDay(i/8)}</h2>
 			            <img src="img/${data.list[i].weather[0].icon}.svg" alt="sunny">
 			            <h3>${(Weather.getTempToFahr(data.list[i].main.temp))} &deg;</h3>
-			            <h3>${data.list[i].weather[0].description}</h3>
+			            <h3>${Weather.capitalize(data.list[i].weather[0].description)}</h3>
 			        </div>
 				`;
-				console.log("length is", data.list[i].weather[0].description);
 			}
 
 			frame.innerHTML = content;
-		} else if (type === 'sixteen-day') {
-			console.log("going to 16 day");
-			frame.innerHTML = `
-				Sixteen Day Coming Soon...
-			`;
+		} else if (type === 'seven-day') {
+			let content = `
+				<div class="row">
+		          <div class="col-md-12"><h2>Forecast For ${data.city.name}</h2></div>
+		        </div>`;
+
+			for (let i = 0; i < data.list.length; i++) {
+				content += `
+					<div class="col-md-3 card">
+			            <h2>${Weather.getDay(i)}</h2>
+			            <img src="img/${data.list[i].weather[0].icon}.svg" alt="sunny">
+			            <h3>${(Weather.getTempToFahr(data.list[i].temp.day))} &deg;</h3>
+			            <h3>${Weather.capitalize(data.list[i].weather[0].description)}</h3>
+			        </div>
+				`;
+			}
+			frame.innerHTML = content;
 		}
 	};
 	return obj;
